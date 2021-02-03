02cf08d9838ba92fae0f4a9093fbfbe4e1995531
lbarrillea <lbarrillea@gmail.com>
2021-02-03 17:31:24 -0600

    create consoleApp intaction with text

--- src/ConsoleApp/consoleAppDemo.java
+++ src/ConsoleApp/consoleAppDemo.java
@@ -7,14 +7,40 @@ package ConsoleApp;
  * @author Louis
  *
  */
-public class consoleAppDemo {
 
+import java.util.Scanner;
+
+
+
+public class consoleAppDemo {
+	
 	/**
 	 * @param args
 	 */
+	
+	
+	
+	
 	public static void main(String[] args) {
+
+		
+		System.out.println("Brewmaster");
+
+		System.out.println("press any key to continue");
+		
+		
+		Scanner in = new Scanner(System.in);
+		in.next();
+		System.out.println("End");
+
+	
+	
 		// TODO Auto-generated method stub
 
+		
+
+
+		
 	}
 
 }
