2039b775d60f736d0cd6c15784cb9e47eee73150
lbarrillea <lbarrillea@gmail.com>
2021-02-03 12:36:53 -0600

    create Eclipse Project and console class

---
+++ src/ConsoleApp/consoleAppDemo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ConsoleApp;
+
+/**
+ * @author Louis
+ *
+ */
+public class consoleAppDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
