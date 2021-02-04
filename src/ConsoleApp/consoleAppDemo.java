3122c1836303c66c1261920cbc19898ef03650f0
lbarrillea <lbarrillea@gmail.com>
2021-02-04 17:03:17 -0600

    changes made

--- src/ConsoleApp/consoleAppDemo.java
+++ src/ConsoleApp/consoleAppDemo.java
@@ -10,37 +10,23 @@ package ConsoleApp;
 
 import java.util.Scanner;
 
-
-
 public class consoleAppDemo {
 	
 	/**
 	 * @param args
 	 */
 	
-	
-	
-	
 	public static void main(String[] args) {
 
-		
 		System.out.println("Brewmaster");
 
-		System.out.println("press any key to continue");
-		
+		System.out.println("press any key to continue");		
 		
 		Scanner in = new Scanner(System.in);
 		in.next();
 		System.out.println("End");
 
-	
-	
 		// TODO Auto-generated method stub
-
-		
-
-
-		
 	}
 
 }
