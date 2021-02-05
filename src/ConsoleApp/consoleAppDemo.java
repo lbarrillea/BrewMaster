a71b83ce2d6ff2298493589708d61420c6d599c3
lbarrillea <lbarrillea@gmail.com>
2021-02-04 19:51:11 -0600

    removed comments

--- src/ConsoleApp/consoleAppDemo.java
+++ src/ConsoleApp/consoleAppDemo.java
@@ -2,31 +2,23 @@
  * 
  */
 package ConsoleApp;
-
 /**
  * @author Louis
  *
  */
-
 import java.util.Scanner;
 
 public class consoleAppDemo {
 	
-	/**
-	 * @param args
-	 */
-	
+
 	public static void main(String[] args) {
 
 		System.out.println("Brewmaster");
-
-		System.out.println("press any key to continue");		
+		
+		System.out.println("Press enter to continue");
 		
 		Scanner in = new Scanner(System.in);
 		in.next();
-		System.out.println("End");
-
-		// TODO Auto-generated method stub
 	}
 
 }
