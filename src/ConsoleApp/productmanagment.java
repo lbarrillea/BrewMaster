1874d044096ad34d79a844834de0477da52359c0
Louis_Barrilleaux <lbarrillea@gmail.com>
2021-03-01 18:24:53 -0600

    the additions are made and the comments removed

---
+++ src/ConsoleApp/productmanagment.java
@@ -0,0 +1,40 @@
+package ConsoleApp;
+
+public class productmanagment {
+	long productid;
+	String productname;
+	String amount;
+	String ingredients;
+	String productdescription;
+	public long getProductid() {
+		return productid;
+	}
+	public void setProductid(long productid) {
+		this.productid = productid;
+	}
+	public String getProductname() {
+		return productname;
+	}
+	public void setProductname(String productname) {
+		this.productname = productname;
+	}
+	public String getAmount() {
+		return amount;
+	}
+	public void setAmount(String amount) {
+		this.amount = amount;
+	}
+	public String getIngredients() {
+		return ingredients;
+	}
+	public void setIngredients(String ingredients) {
+		this.ingredients = ingredients;
+	}
+	public String getProductdescription() {
+		return productdescription;
+	}
+	public void setProductdescription(String productdescription) {
+		this.productdescription = productdescription;
+	}
+
+}
